0ec0e907de915497f497c7102c94ab496e257c3f
C-W-Z <cwz.me@proton.me>
2026-06-01 12:36:14 +0800

    feat: add 3 cards & 1 power cards: OuterSurgery, EquivalentExchange, FleshDetached power: FleshDetachedPower

---
+++ MorimensDollCode/Cards/EquivalentExchange.cs
@@ -0,0 +1,40 @@
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.ValueProps;
+using MorimensDoll.Characters;
+using STS2RitsuLib.Interop.AutoRegistration;
+
+namespace MorimensDoll.Cards;
+
+[RegisterCard(typeof(DollCardPool))]
+[RegisterCharacterStarterCard(typeof(Doll), 1)]
+public sealed class EquivalentExchange() : AbstractDollCard(3, CardType.Skill, CardRarity.Common, TargetType.Self)
+{
+    protected override HashSet<CardTag> CanonicalTags => [];
+
+    protected override IEnumerable<DynamicVar> CanonicalVars => [
+        new HealVar(5m),
+        // CalculatedBlock = 0 + 2 * 手牌數
+        new CalculationBaseVar(0m),
+        new CalculationExtraVar(2m),
+        new CalculatedBlockVar(ValueProp.Move).WithMultiplier((card, _) => PileType.Hand.GetPile(card.Owner).Cards.Count)
+    ];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        await CreatureCmd.TriggerAnim(Owner.Creature, Doll.State.Skill2, Owner.Character.CastAnimDelay);
+        var blockAmount = DynamicVars.CalculatedBlock.Calculate(cardPlay.Target);
+        IEnumerable<CardModel> cards = PileType.Hand.GetPile(Owner).Cards;
+        await CardCmd.Discard(choiceContext, cards);
+        await CreatureCmd.Heal(Owner.Creature, DynamicVars.Heal.BaseValue + blockAmount, true);
+        await CreatureCmd.GainBlock(Owner.Creature, blockAmount, DynamicVars.CalculatedBlock.Props, cardPlay);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.CalculationExtra.UpgradeValueBy(1m);
+    }
+}
---
+++ MorimensDollCode/Cards/OuterSurgery.cs
@@ -0,0 +1,32 @@
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.Powers;
+using MorimensDoll.Characters;
+using STS2RitsuLib.Interop.AutoRegistration;
+
+namespace MorimensDoll.Cards;
+
+[RegisterCard(typeof(DollCardPool))]
+[RegisterCharacterStarterCard(typeof(Doll), 1)]
+public sealed class OuterSurgery() : AbstractDollCard(2, CardType.Skill, CardRarity.Common, TargetType.AllEnemies)
+{
+    protected override HashSet<CardTag> CanonicalTags => [];
+
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new HealVar(5m), new PowerVar<WeakPower>(1m)];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        ArgumentNullException.ThrowIfNull(CombatState);
+        await CreatureCmd.TriggerAnim(Owner.Creature, Doll.State.Skill1, Owner.Character.CastAnimDelay);
+        await CreatureCmd.Heal(Owner.Creature, DynamicVars.Heal.BaseValue, true);
+        // all enemies
+        await PowerCmd.Apply<WeakPower>(choiceContext, CombatState.HittableEnemies, DynamicVars.Weak.BaseValue, Owner.Creature, this);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Weak.UpgradeValueBy(1m);
+    }
+}
