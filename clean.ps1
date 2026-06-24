dfbd42d46d69f221224c161462f0a030362e6f7c
C-W-Z <cwz.me@proton.me>
2026-06-24 18:25:50 +0800

    chore: update project settings

--- clean.ps1
+++ clean.ps1
@@ -1,2 +1,6 @@
-gci . -fi "*.uid" -r | ri -Force
-gci . -fi "*.import" -r | ri -Force
+Get-ChildItem . -fi "*.uid" -r | Remove-Item -Force
+Get-ChildItem . -fi "*.import" -r | Remove-Item -Force
+Get-ChildItem . -fi "obj" -r -Directory | Remove-Item -Recurse -Force
+Get-ChildItem . -fi "bin" -r -Directory | Remove-Item -Recurse -Force
+Get-ChildItem . -fi ".godot" -r -Directory | Remove-Item -Recurse -Force
+# You need to reopen the project by Godot to reimport resources & packages, otherwise the next dotnet build wont success
