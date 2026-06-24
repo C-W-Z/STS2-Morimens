9e15336c6ba23c7d331f54fadfa46adb709b7e0a
C-W-Z <cwz.me@proton.me>
2026-06-24 12:16:25 +0800

    chore: update submodule in workflow feat: implement card ManikinOfOblivion (but ManikinOfOblivionPower not implemented yet)

--- Characters/Shared/Cards/Wheels/ManikinOfOblivion.cs
+++ Characters/Shared/Cards/Wheels/ManikinOfOblivion.cs
@@ -1,6 +1,9 @@
+using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using Morimens.Characters.Shared.Definition;
+using Morimens.Characters.Shared.Powers;
 using Morimens.Core.Card;
 using STS2RitsuLib.Interop.AutoRegistration;
 
@@ -11,8 +14,12 @@ public sealed class ManikinOfOblivion() : AbstractWheelCard(CardRarity.Rare)
 {
     public override WheelFrameType WheelType => WheelFrameType.Chaos;
 
+    protected override IEnumerable<DynamicVar> CanonicalVars => [
+        new PowerVar<ManikinOfOblivionPower>(20m)
+    ];
+
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
-
+        await PowerCmd.Apply<ManikinOfOblivionPower>(choiceContext, Owner.Creature, DynamicVars["ManikinOfOblivionPower"].BaseValue, null, this);
     }
 }
